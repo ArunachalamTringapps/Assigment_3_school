4c60c8593063ff04b401cdc72e7f1673361ba1b3
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-02-28 06:03:53 +0530

    updated withour code smell

--- src/main/java/org/example/BasicSchool.java
+++ src/main/java/org/example/BasicSchool.java
@@ -91,27 +91,3 @@ class BasicSchool {
 
     }
 }
-
-class Grade {
-    private final String temp;
-    Logger l=Logger.getLogger(BasicSchool.class.getName());
-
-    Grade(String temp) {
-        this.temp = temp;
-    }
-
-    public double gradeing() {
-        double gradetemp = 0;
-        switch (temp) {
-            case "A" -> gradetemp = 4;
-            case "B" -> gradetemp = 3;
-            case "C" -> gradetemp = 2;
-            case "D" -> gradetemp = 1;
-            case "F" -> gradetemp = 0;
-            default -> l.info("Enter the correct Grade");
-        }
-
-        return gradetemp;
-
-    }
-}
